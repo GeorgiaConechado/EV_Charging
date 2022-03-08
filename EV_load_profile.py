df7605ff74460cd955c3e9455fe95139413bba11
Georgia Conechado <gconechado@gmail.com>
2022-03-08 17:42:35 +1100

    Changed traffic variation to normal dist

--- EV_load_profile.py
+++ EV_load_profile.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from doctest import Example
 import numpy as np
 import pandas as pd
 
@@ -10,15 +11,15 @@ EV_inputs_example = { #EV model details example
 
 traffic_inputs_2020 = {
     'year': 2030, # the year for analysis
-    'traffic': 28,  #forecast mean daily EV traffic volume (two way)
-    'traffic_var':0.4, #perc variation in traffic each day
+    'traffic_mu': 28,  #forecast mean daily EV traffic volume (two way)
+    'traffic_sigma':5, #standard deviation for the traffic volume
     'stop_prob':0.2, #probability of car passing by to stop
     'hourly_weight':[2,2,1,1,1,1,3,7,14,20,23,27,26,25,23,17,13,8,5,3,3,2,2,2], #rough weightings for probability of EV arriving at each time of day
 }
 
 """
 Determining the EV arrival times from the traffic data is done in three steps:
-1. Variation of daily traffic volume from given mean traffic, using discrete uniform distribution
+1. Variation of daily traffic volume from given mean traffic, using normal distribution
 2. Probability each EV will stop
 3. Generate arrival time for each of these EVs, using manually input probability weightings from given hourly profile
 (If not using a given hourly profile could make this normal distribution instead)
@@ -43,9 +44,7 @@ def load_profile(EV_inputs, traffic_inputs): #generates timeseries csv
     return    
 
 def randomise_travel(traffic_inputs): #simplest version - random times of day
-    min_traffic = np.floor(traffic_inputs['traffic']*(1-traffic_inputs['traffic_var']))
-    max_traffic = np.ceil(traffic_inputs['traffic']*(1+traffic_inputs['traffic_var'])) 
-    traffic = np.random.randint(min_traffic,max_traffic+1) #add 1 to max since randint returns random integers from low (inclusive) to high (exclusive)
+    traffic = np.round(np.random.normal(loc= traffic_inputs['traffic_mu'], scale = traffic_inputs['traffic_sigma']))
     arrival_times = np.random.choice(24,size=int(traffic*traffic_inputs['stop_prob']),p=probability(traffic_inputs['hourly_weight']))
     return arrival_times
 
