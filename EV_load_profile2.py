73782f703ef58fec153b062d690146ba13849f27
Georgia Conechado <gconechado@gmail.com>
2022-04-26 12:10:52 +1000

    fixed issue where each day in year had same profile

--- EV_load_profile2.py
+++ EV_load_profile2.py
@@ -1,16 +1,17 @@
 import datetime as dt
 from doctest import Example
+from turtle import shape
 import numpy as np
 import pandas as pd
 
+hourly_weight = [2,2,1,1,1,1,3,7,14,20,23,27,26,25,23,17,13,8,5,3,3,2,2,2] #example taken from stuart highway evernergi report
 
 def arrival_times(traffic_mu, traffic_sigma, hourly_weight, stop_prob = 1):
-    traffic = np.round(np.random.normal(loc= traffic_mu, scale = traffic_sigma))
-    arrival_times = np.random.choice(24,size=int(traffic*stop_prob),p=probability(hourly_weight))
-
     yearly_arrival_times = []    
     for i in range (365):
         daily = []
+        traffic = np.round(np.random.normal(loc= traffic_mu, scale = traffic_sigma))
+        arrival_times = np.random.choice(24,size=int(traffic*stop_prob),p=probability(hourly_weight))
         for j in range (24):
             daily.append(np.count_nonzero(arrival_times == j))
         yearly_arrival_times.append(daily)
@@ -26,13 +27,14 @@ def probability(weightings):
 
 
 def char_profile (yearly_arrival_times, num_chargers):
-    char = []
+    char_profile = []
     current_wait_EV = []
-    prev_wait_EV = []
+    total_prev_wait_EV = []
 
     for i in range(len(yearly_arrival_times)):
         daily_char = []
         daily_times = yearly_arrival_times[i]
+        prev_wait_EV = []
         for j in range(len(daily_times)):
             #print(j)
             arriving_EVs = daily_times[j]
@@ -55,13 +57,35 @@ def char_profile (yearly_arrival_times, num_chargers):
             for n in range (arrive_EV_to_wait):
                 current_wait_EV.append(1) #add the new waiting EVs with a wait time of 1 hour
             #print('final current waiting EV', current_wait_EV)
+        total_prev_wait_EV.append(prev_wait_EV)
+        char_profile.append(daily_char)
+    #print(len(total_prev_wait_EV[0]))
+    return char_profile, total_prev_wait_EV
 
-        char.append(daily_char)
-    print(prev_wait_EV)
-    return char, prev_wait_EV
+#char_profile(arrival_times(30,5,hourly_weight),3)
 
-#char_profile(arrival_times(30,5,[2,2,1,1,1,1,3,7,14,20,23,27,26,25,23,17,13,8,5,3,3,2,2,2]),3)
+def optimise_chargers (yearly_arrival_times, init_chargers, allow_max_wait_time, allow_max_daily_wait_EV):
+    '''
+    allow_max_wait_time = maxmimum allowable length of time for any given EV to wait in hours
+    allow_max_daily_wait_EV = maximum allowable number of EVs which have to wait over any given day
+    '''
+    num_chargers = init_chargers
+    wait_EV = char_profile(yearly_arrival_times,num_chargers)[1]
+    # print(wait_EV)
+    max_wait_time = np.max(np.max(np.max(wait_EV)))
+    # print(max_wait_time)
+    # max_wait_time = max([item for sublist in wait_EV for item in sublist])
+    # print(max_wait_time)
 
-# def optimise_chargers (yearly_arrival_times, init_chargers, acceptable_wait):
+    allow_max_daily_wait_EV = 0
+    # for i in range (365):
+    #     print(i)
+    #     print(wait_EV[i])
     
-#     return num_chargers
+    #while 
+
+    return num_chargers
+
+yearly_arrival_times = arrival_times(30,5,hourly_weight)
+print(yearly_arrival_times)
+optimise_chargers(yearly_arrival_times,3,1,5)
